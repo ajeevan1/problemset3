a695c8acef3a655918f2c4dab3c81b4bc94de25b
ajeevan1 <100706518+ajeevan1@users.noreply.github.com>
2022-10-21 12:22:20 -0400

    adding comments to problem 3

--- problem3.py
+++ problem3.py
@@ -2,18 +2,18 @@
 
 file = open('CO-OPS.csv')
 next(file)
-max_change = -1.7976931348623517e+308
+max_change = -1.7976931348623517e+308 # arbitrary high float value
 date_change = ""
-prev_level = -1.7976931348623517e+308
+prev_level = -1.7976931348623517e+308 # arbitrary high float value
 for line in file:
-   column = line.split(",")
-   curr_level = column[1]
+   column = line.split(",") # splits file to isolate water level column
+   curr_level = column[1] 
    datetime = column[0]
-   if(curr_level == ""): curr_level = prev_level
+   if(curr_level == ""): curr_level = prev_level # accounts for blank value in column
    if(prev_level == -1.7976931348623517e+308):
       prev_level = float(curr_level)
    else:
-      if(float(curr_level) - prev_level >= max_change):
+      if(float(curr_level) - prev_level >= max_change): # goes through file to find max difference between values
          max_change = float(curr_level) - prev_level
          prev_level = float(curr_level)
          date_change = datetime
